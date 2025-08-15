1caba8fd35b53237860772e670f799f2ed626fb3
kainat20 <sana.kainat@devsinc.com>
2025-08-16 00:42:07 +0500

    feat: add terraform configuration and update gitignore
    - Add terraform infrastructure configuration
    - Update CI workflow
    - Add prettierignore file
    - Update gitignore to exclude terraform state and provider files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,18 @@
 # In this file put the variables related to the deployment
-variable "variable_name" {
-    type = "type",
-    description = "Description"
+variable "env" {
+    type = string
+    validation {
+      condition = contains(["devel","stage","prod"],var.env)
+      error_message = "env must be devel , stage or prod"
+    }
+
+}
+
+variable "account_suffix" {
+      type = string
+}
+
+variable "tags"{
+ type = map(string)
+ default = {}
 }
